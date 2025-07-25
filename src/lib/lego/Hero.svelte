9ee1c756c32690d5bd35e66ffb39b5ac7fbdc0f3
doromiert <85891552+NRDsstuff@users.noreply.github.com>
2025-07-25 14:14:54 +0200

    beta edition

--- src/lib/lego/Hero.svelte
+++ src/lib/lego/Hero.svelte
@@ -15,12 +15,53 @@
             <h><b>doromiert</b> /dor-om-iert/</h>
             <p>Noun. The one that creates.</p>
         </div>
+        <div class="beta">beta edition!</div>
     </div>
 </section>
 
 <style lang="scss">
     @use '$lib/style/variables.scss' as v;
 	@use '$lib/style/colors.scss' as c;
+    @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+    
+    .beta{
+        font-size: 72px;
+        position: absolute;
+        bottom: 400px;
+        right: 500px;
+        color: #ff0000;
+        rotate: -20deg;
+        font-family: "Comic Neue";
+        font-weight: bold;
+        animation: gay 6s ease-in-out;
+    }
+    @keyframes gay {
+            0% {
+                transform: rotate(-10deg);
+                color: #ff0000; /* Red */
+            }
+            16% {
+                color: #ff7f00; /* Orange */
+            }
+            33% {
+                transform: rotate(-20deg);
+                color: #ffff00; /* Yellow */
+            }
+            50% {
+                color: #00ff00; /* Green */
+            }
+            66% {
+                transform: rotate(-10deg);
+                color: #0000ff; /* Blue */
+            }
+            83% {
+                color: #4b0082; /* Indigo */
+            }
+            100% {
+                transform: rotate(-20deg);
+                color: #9400d3; /* Violet */
+            }
+        }
 
     .pageLego{
         background-color: c.$blux-1;
